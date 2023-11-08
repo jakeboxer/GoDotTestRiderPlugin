a472f87cade7eca0bf3b558799eff01b7fbda4d8
Jake Card <jake@megacrit.com>
2023-11-08 12:29:17 -0500

    Add initial project

---
+++ src/dotnet/ReSharperPlugin.GoDotTestRiderPlugin.Tests/TestEnvironment.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.Feature.Services;
+using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.ReSharper.TestFramework;
+using JetBrains.TestFramework;
+using JetBrains.TestFramework.Application.Zones;
+using NUnit.Framework;
+
+[assembly: Apartment(ApartmentState.STA)]
+
+namespace ReSharperPlugin.GoDotTestRiderPlugin.Tests
+{
+    [ZoneDefinition]
+    public class GoDotTestRiderPluginTestEnvironmentZone : ITestsEnvZone, IRequire<PsiFeatureTestZone>, IRequire<IGoDotTestRiderPluginZone> { }
+
+    [ZoneMarker]
+    public class ZoneMarker : IRequire<ICodeEditingZone>, IRequire<ILanguageCSharpZone>, IRequire<GoDotTestRiderPluginTestEnvironmentZone> { }
+
+    [SetUpFixture]
+    public class GoDotTestRiderPluginTestsAssembly : ExtensionTestEnvironmentAssembly<GoDotTestRiderPluginTestEnvironmentZone> { }
+}
